9228bdc03cf54d21794b45a8a985d7724e5aac85
Atsushi <shimarisu.araiguma@gmail.com>
2020-07-05 14:48:28 +0900

    dynamic-search-form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Query } from 'react-apollo';
 import client from './client';
 import { SEARCH_REPOSITORIES } from './graphql';
 
-const VARIABLES = {
+const DEFAULT_STATE = {
   first: 5,
   after: null,
   last: null,
@@ -13,11 +13,23 @@ const VARIABLES = {
 };
 
 const App = () => {
-  const [variable, setVariable] = useState(VARIABLES);
+  const [variable, setVariable] = useState(DEFAULT_STATE);
   const { query, first, last, before, after } = variable;
+
+  const handleChange = (e) => {
+    setVariable({
+      ...variable,
+      query: e.target.value,
+    });
+  };
+
+  console.log({ query });
 
   return (
     <ApolloProvider client={client}>
+      <form>
+        <input value={query} onChange={handleChange} />
+      </form>
       <Query
         query={SEARCH_REPOSITORIES}
         variables={{ query, first, last, before, after }}
